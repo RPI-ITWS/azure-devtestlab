07519504c4aca84e732d97ce88686928dd7f3c57
Joshua Cherry <cherrj2@rpi.edu>
2022-08-26 13:43:49 -0400

    added ITWS 1100 and 2110 artifacts

---
+++ Artifacts/ITWS-1100/itws-2110.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This Azure DevTest Labs artifact installs various requirements for the ITWS 2110 course at RPI.
+#
+# NOTE: Intended for use by the Azure DevTest Lab artifact system.
+#
+set -e
+
+echo "----------- Script Complete -----------"
---
+++ Artifacts/ITWS-2110/itws-1100.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# This Azure DevTest Labs artifact installs various requirements for the ITWS 1100 course at RPI.
+#
+# NOTE: Intended for use by the Azure DevTest Lab artifact system.
+#
+set -e
+
+echo "----------- Script Complete -----------"
